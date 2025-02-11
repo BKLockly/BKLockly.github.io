d2d0d8b9662b0336f52bbdd7e5956cd27d8728cb
Lockly <113906689+BKLockly@users.noreply.github.com>
2025-02-11 15:13:04 +0800

    docs: 更新网站统计代码

--- gitbook/gitbook-plugin-page-footer-ex/lib/plugin.js
+++ gitbook/gitbook-plugin-page-footer-ex/lib/plugin.js
@@ -2,9 +2,11 @@
  * @Author: Lockly
  * @Date: 2025-01-23 10:13:48
  * @LastEditors: Lockly
- * @LastEditTime: 2025-02-04 22:11:36
+ * @LastEditTime: 2025-02-11 15:02:21
  */
-document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3KtnugBE6rFgFEk4\",ck:\"3KtnugBE6rFgFEk4\"});</script>")
+
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3LE5fuDI15UWWSyI\",ck:\"3LE5fuDI15UWWSyI\"});</script>")
+
 module.exports = function(book, page) {
     /**
      * 在package.json中配置的默认值，这里可以直接使用
